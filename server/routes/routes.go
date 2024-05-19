983559e08f5d6c5efdc131054136716ac24f25a1
differencialx <differencialx@gmail.com>
2024-05-19 05:13:12 +0300

    Add frontend

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,10 +1,21 @@
 package routes
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(server *gin.Engine) {
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},            // Allow specific origins
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},     // Allow specific HTTP methods
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"}, // Allow specific headers
+		ExposeHeaders:    []string{"Content-Length"},                   // Expose specific headers
+		AllowCredentials: true,                                         // Allow credentials
+		MaxAge:           12 * time.Hour,                               // Max age for the preflight request
+	}))
 	api := server.Group("/api")
 	api.GET("/todos", getTodoList)
 	api.POST("/todos", postTodo)
